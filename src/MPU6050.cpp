b71af196da835b18507f450eba0f3db1075a97ee
younyumi <yyumi0701@g.skku.edu>
2024-08-26 00:43:20 +0900

    mpu6050-revise

--- src/MPU6050.cpp
+++ src/MPU6050.cpp
@@ -2,57 +2,53 @@
 #include <Wire.h>
 #include <Arduino.h>
 
-MPU6050::MPU6050() : mpu() {}
+MPU6050::MPU6050() : mpu() {}  //mpu 객체
 
 void MPU6050::begin() {
-    if (!mpu.begin()) {
-        Serial.println("Failed to find MPU6050 chip");
-        while (1) {
+    if(!mpu.begin()) {
+        Serial.println("Failed to find MPU6050");   //센서 연결 오류코드
+        while(1){
             delay(10);
         }
     }
 
-    Serial.println("MPU6050 Found!");
-
-    mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-    mpu.setGyroRange(MPU6050_RANGE_250_DEG);
-    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);  //가속도 인식 범위 : 플마8G로 설정함, 테스트해보고 수정
+    mpu.setGyroRange(MPU6050_RANGE_1000_DEG);       //너무 넓으면 분해능 저하
+    mpu.setFilterBandwidth(MPU6050_BAND_94_HZ);  //주파수 범위(44,94,184,260) 너무 높으면 노이즈많이포함, 너무 낮으면 반응속도떨어짐
 }
 
 void MPU6050::readSensors() {
-    sensors_event_t a, g, temp;
+    sensors_event_t a, g,temp;
     mpu.getEvent(&a, &g, &temp);
 
-    // 가속도 출력 (m/s^2)
-    Serial.print("Acceleration X: ");
+    Serial.print("Acceleration X: ");    //가속도랑 roll, pitch만 사용할듯
     Serial.print(a.acceleration.x);
-    Serial.print(" m/s^2, Y: ");
+    Serial.print("Acceleration Y: ");
     Serial.print(a.acceleration.y);
-    Serial.print(" m/s^2, Z: ");
+    Serial.print("Acceleration Z: ");
     Serial.print(a.acceleration.z);
-    Serial.println(" m/s^2");
+    Serial.println("m/s^2");
 
     // 각속도 출력 (rad/s)
-    Serial.print("Rotation X: ");
-    Serial.print(g.gyro.x);
-    Serial.print(" rad/s, Y: ");
-    Serial.print(g.gyro.y);
-    Serial.print(" rad/s, Z: ");
-    Serial.print(g.gyro.z);
-    Serial.println(" rad/s");
+    // Serial.print("Rotation X: ");
+    // Serial.print(g.gyro.x);
+    // Serial.print(" rad/s, Y: ");
+    // Serial.print(g.gyro.y);
+    // Serial.print(" rad/s, Z: ");
+    // Serial.print(g.gyro.z);
+    // Serial.println(" rad/s");
 
-    // Pitch와 Roll 각도 계산 및 출력
-    float pitch = atan(a.acceleration.x / sqrt(pow(a.acceleration.y, 2) + pow(a.acceleration.z, 2))) * 180.0 / PI;
-    float roll = atan(a.acceleration.y / sqrt(pow(a.acceleration.x, 2) + pow(a.acceleration.z, 2))) * 180.0 / PI;
-
-    Serial.print("Pitch: ");
-    Serial.print(pitch);
-    Serial.print(" degrees, ");
+    float roll = atan(a.acceleration.x / sqrt(pow(a.acceleration.y,2)+pow(a.acceleration.z,2)))*180.0/PI;
+    float pitch = atan(a.acceleration.y / sqrt(pow(a.acceleration.x,2)+pow(a.acceleration.z,2)))*180.0/PI;
 
     Serial.print("Roll: ");
     Serial.print(roll);
-    Serial.println(" degrees");
+    Serial.print(" deg, ");
 
-    Serial.println(""); // 출력 간격을 주기 위해 빈 줄 추가
-    delay(500);         // 0.5초 간격으로 데이터 출력
+    Serial.print("Pitch: ");
+    Serial.print(pitch);
+    Serial.print(" deg");
+
+    Serial.println("");
+    delay(500);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
-#include <Arduino.h>
+#include  <Arduino.h>
 #include "MPU6050.h"
 
-MPU6050 mpuModule;
+MPU6050 mpuModule; //  MPU6050 클래스의 mpuModule객체 생성
 
 void setup() {
     Serial.begin(115200);
-    mpuModule.begin(); // MPU6050 초기화
+    mpuModule.begin();
 }
 
 void loop() {
-    mpuModule.readSensors(); // 센서 데이터 읽기 및 출력
-    Serial.println("");      // 출력 간격을 주기 위해 빈 줄 추가
-    delay(500);              // 0.5초 간격으로 데이터 출력
+    mpuModule.readSensors();
+    Serial.println("");
+    delay(500);
 }
