1fce6ff1e155c3c58c8f8960f2bba27ffe96679f
younyumi <yyumi0701@g.skku.edu>
2024-08-26 04:38:15 +0900

    csv-add

--- csv_data.py
+++ csv_data.py
@@ -1,27 +1,9 @@
-# import serial
-# import csv
-# import time
 
-# ser = serial.Serial('/dev/ttyACM1', 115200)  
-
-# with open('data.csv', mode='w', newline='') as file:
-#     writer = csv.writer(file)
-#     writer.writerow(['Timestamp', 'Distance'])  # 헤더 
-
-#     while True:
-#         line = ser.readline().decode('utf-8').strip()
-#         if line:
-#             print(line)  # 시리얼 모니터에 출력된 데이터 확인
-#             parts = line.split(':')
-#             if len(parts) == 2:
-#                 distance = parts[1].strip().replace("cm", "").strip()
-#                 timestamp = time.time()
-#                 writer.writerow([timestamp, distance])
-import serial
+import serial  
 import csv
 import time
 
-ser = serial.Serial('/dev/ttyACM1', 115200)  
+ser = serial.Serial('/dev/ttyACM1', 115200)  #포트설정(꼭확인해야함)
 
 with open('data.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
