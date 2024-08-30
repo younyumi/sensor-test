8dc2b1e8aa435b653b3210bdf280fdf245af8446
younyumi <yyumi0701@g.skku.edu>
2024-08-30 14:58:19 +0900

    revise-0830

--- src/ShockSensor.cpp
+++ src/ShockSensor.cpp
@@ -1,34 +1,27 @@
 #include <Arduino.h>
 
-const int sensorPins[4] = {2,3,4,5};
-unsigned long DetectedTimes[4] = {0,0,0,0};
-const unsigned long debounceTime = 500; //debounce 시간 설정-> 테스트 해보면서 결정해야할듯
+const int sensorPin = 2; // Using pin 2 for the SW-420 sensor
+unsigned long lastDetectedTime = 0;
+const unsigned long debounceTime = 500; // Debounce time in milliseconds
 
-void setup(){
-  Serial.begin(9600);
-  for (int i=0;i<4; i++){
-    pinMode(sensorPins[i], INPUT);
-  }
+void setup() {
+  Serial.begin(115200);
+  pinMode(sensorPin, INPUT);
 }
 
-void loop(){
-  for (int i=0; i<4; i++){
-    int sensorValue = digitalRead(sensorPins[i]);
+void loop() {
+  int sensorValue = digitalRead(sensorPin);
 
-    if(sensorValue == LOW){
-      unsigned long currentTime = millis();
-      if(currentTime -DetectedTimes[i] > debounceTime) { //debounce 시간 지나고 충격감지됐을때 유효한걸로 처리
-        float time = currentTime / 1000.0;
-        
-        Serial.print("Shock detected on sensor");
-        Serial.print(i+1);
-        Serial.print(" at time: ");
-        Serial.print(time, 3);
-        Serial.print(" s");
-        DetectedTimes[i] = currentTime;
-      }
+  if (sensorValue == HIGH) { // LOW indicates shock detected
+    unsigned long currentTime = millis();
+    if (currentTime - lastDetectedTime > debounceTime) { // Check if debounce time has passed
+      float time = currentTime / 1000.0;
+
+      Serial.print("Shock detected at time: ");
+      Serial.print(time, 3);
+      Serial.println(" s");
+      lastDetectedTime = currentTime;
     }
   }
-  delay(50);
+  delay(100); // Small delay to reduce CPU usage
 }
-
