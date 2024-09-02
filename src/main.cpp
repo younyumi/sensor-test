a072a3affe1ed927c8d399775cdaff3747480213
younyumi <yyumi0701@g.skku.edu>
2024-09-03 02:56:19 +0900

    revise

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,108 @@
 #include <Arduino.h>
-#include "GPS.h"
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
 
-GPSModule gpsModule(4, 3); // RX, TX 핀 번호
+// Define which pins you will use on the Arduino to communicate with your GPS
+#define RXPIN 6
+#define TXPIN 5
 
+// Set this value equal to the baud rate of your GPS
+#define GPSBAUD 9600
+
+// Create an instance of the TinyGPSPlus object
+TinyGPSPlus gps;
+
+// Initialize the SoftwareSerial library to the pins you defined above
+SoftwareSerial uart_gps(RXPIN, TXPIN);
+
+// Function prototypes
+void displayInfo();
+
+// Setup function
 void setup() {
-    Serial.begin(115200);
-    gpsModule.begin();
+  // Initialize serial communication with the terminal
+  Serial.begin(9600);
+  // Initialize serial communication with the GPS module
+  uart_gps.begin(GPSBAUD);
+
+  // Print initial messages
+  Serial.println();
+  Serial.println("GPS Shield QuickStart Example Sketch");
+  Serial.println("       ...waiting for lock...           ");
+  Serial.println();
 }
 
+// Main loop function
 void loop() {
-    gpsModule.update();
-    if (gpsModule.locationIsValid()) {
-        Serial.print("Latitude: ");
-        Serial.println(gpsModule.getLatitude(), 6);
-        Serial.print("Longitude: ");
-        Serial.println(gpsModule.getLongitude(), 6);
-    } else {
-        Serial.println("Location not valid.");
+  // While there is data available on the RX pin...
+  while (uart_gps.available()) {
+    // Read the data into a variable
+    char c = uart_gps.read();
+    
+    // If there is a new valid sentence...
+    if (gps.encode(c)) {
+      // Display the GPS information
+      displayInfo();
     }
-    delay(1000);
+  }
+}
+
+// Function to display GPS information
+void displayInfo() {
+  if (gps.location.isValid()) {
+    Serial.print("Lat/Long: ");
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(", ");
+    Serial.println(gps.location.lng(), 6);
+  } else {
+    Serial.println("Location: INVALID");
+  }
+
+  if (gps.date.isValid()) {
+    Serial.print("Date: ");
+    Serial.print(gps.date.month());
+    Serial.print("/");
+    Serial.print(gps.date.day());
+    Serial.print("/");
+    Serial.println(gps.date.year());
+  } else {
+    Serial.println("Date: INVALID");
+  }
+
+  if (gps.time.isValid()) {
+    Serial.print("Time: ");
+    Serial.print(gps.time.hour());
+    Serial.print(":");
+    Serial.print(gps.time.minute());
+    Serial.print(":");
+    Serial.print(gps.time.second());
+    Serial.print(".");
+    Serial.println(gps.time.centisecond());
+  } else {
+    Serial.println("Time: INVALID");
+  }
+
+  if (gps.altitude.isValid()) {
+    Serial.print("Altitude (meters): ");
+    Serial.println(gps.altitude.meters());
+  } else {
+    Serial.println("Altitude: INVALID");
+  }
+
+  if (gps.course.isValid()) {
+    Serial.print("Course (degrees): ");
+    Serial.println(gps.course.deg());
+  } else {
+    Serial.println("Course: INVALID");
+  }
+
+  if (gps.speed.isValid()) {
+    Serial.print("Speed (kmph): ");
+    Serial.println(gps.speed.kmph());
+  } else {
+    Serial.println("Speed: INVALID");
+  }
+
+  Serial.println();
+  delay(1000);  // Adjust delay as needed
 }
